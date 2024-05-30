b7ce4778fabb3249803f87ed0d718a98e5b2047e
Amirhossein Ayoubi <amirhosseinayoubi83@gmail.com>
2024-05-30 14:00:54 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/*
+// tamrin zoj va fard boodan 
+
+var number = Number(prompt("enter your number to check it :"))
+
+if( number % 2 == 0)
+{
+    alert("your number is \"زوج\" ")
+}else
+{
+    alert("your number is \"فرد \" ")
+}
+// -----------------------------------------------------------------------------------------
+*/
+
